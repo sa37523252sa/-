43277f025ca0ec033f3c279841e3ae619b1b9f7f
sa37523252sa <ghh8dd6rgt@privaterelay.appleid.com>
2025-12-05 22:56:52 +0800

    Add files via upload

--- routes/products.js
+++ routes/products.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const { auth, admin } = require("../middleware/authMiddleware");
+const { addProduct, listProducts, searchProducts } = require("../controllers/productController");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
+router.post("/add", auth, admin, upload.single("image"), addProduct);
+router.get("/list", listProducts);
+router.get("/search", searchProducts);
+
+module.exports = router;
