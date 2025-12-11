157a6a45ce0f671d70ffa273eabe3db109faba80
sa37523252sa <ghh8dd6rgt@privaterelay.appleid.com>
2025-12-11 17:00:46 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/authController");
+const authController = require("./authController");
+
 
 router.post("/register", register);
 router.post("/login", login);
